fec124330c628b0891559b1076fe3fc26be06c07
andre <andreas.stathop@gmail.com>
2019-12-20 12:46:57 +0200

    Edit YiannisController & create View for About

--- WebApplication1/WebApplication1/Controllers/YiannisController.cs
+++ WebApplication1/WebApplication1/Controllers/YiannisController.cs
@@ -13,5 +13,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
     }
 }
