47cc6a33366ea55c48e340895e70839271e9c20f
andre <andreas.stathop@gmail.com>
2019-12-20 15:22:32 +0200

    Edit class Andreas

--- WebApplication1/WebApplication1/Models/Andreas.cs
+++ WebApplication1/WebApplication1/Models/Andreas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,7 @@ namespace WebApplication1.Models
 {
     public class Andreas
     {
+        [Required]
+        public int ID { get; set; }
     }
 }
