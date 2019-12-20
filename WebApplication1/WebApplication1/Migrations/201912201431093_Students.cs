fffca2ec97742bd1b497a27a8e99cda13e38fd0e
xtijohn <ypoulopoulos@hotmail.com>
2019-12-20 16:34:20 +0200

    Database

---
+++ WebApplication1/WebApplication1/Migrations/201912201431093_Students.cs
@@ -0,0 +1,26 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Students : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region DbSets
+        public DbSet<Student> Students { get; set; }
+        #endregion
+
+        #region Constructors
+        public ApplicationDbContext() : base("SampleConnection")
+        {
+
+        }
+        #endregion
+
+    }
+}
